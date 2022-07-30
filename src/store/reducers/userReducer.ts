c3eee9f0f84045544b907f38d37e768300efc6d2
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-30 19:56:59 +0500

    refactor(userReducer): move types to a separate file

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,30 +1,4 @@
-enum UserActionTypes {
-  FETCH_USERS = 'FETCH_USERS',
-  FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
-  FETCH_USERS_ERROR = 'FETCH_USERS_ERROR',
-}
-
-interface FetchUserAction {
-  type: UserActionTypes.FETCH_USERS;
-};
-
-interface FetchUserSuccessAction {
-  type: UserActionTypes.FETCH_USERS_SUCCESS;
-  payload: any[];
-};
-
-interface FetchUserErrorAction {
-  type: UserActionTypes.FETCH_USERS_ERROR;
-  payload: string;
-};
-
-type UserAction = FetchUserAction | FetchUserSuccessAction | FetchUserErrorAction;
-
-interface UserState {
-  users: any[];
-  loading: boolean;
-  error: null | string;
-};
+import { UserAction, UserActionTypes, UserState } from "../../types/user";
 
 const initialState: UserState = {
   users: [],
@@ -32,7 +6,7 @@ const initialState: UserState = {
   error: null,
 };
 
-export const userReducer = (state = initialState, action:UserAction):UserState => {
+export const userReducer = (state = initialState, action:UserAction): UserState => {
   switch (action.type) {
     case UserActionTypes.FETCH_USERS:
       return {loading: true, error: null, users: []};
