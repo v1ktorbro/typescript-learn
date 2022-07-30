0bc366315c7cba3759cba7e5223ba18de646d18d
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-30 19:50:58 +0500

    feat(App): initial file structure and create userReduser

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,46 @@
+enum UserActionTypes {
+  FETCH_USERS = 'FETCH_USERS',
+  FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
+  FETCH_USERS_ERROR = 'FETCH_USERS_ERROR',
+}
+
+interface FetchUserAction {
+  type: UserActionTypes.FETCH_USERS;
+};
+
+interface FetchUserSuccessAction {
+  type: UserActionTypes.FETCH_USERS_SUCCESS;
+  payload: any[];
+};
+
+interface FetchUserErrorAction {
+  type: UserActionTypes.FETCH_USERS_ERROR;
+  payload: string;
+};
+
+type UserAction = FetchUserAction | FetchUserSuccessAction | FetchUserErrorAction;
+
+interface UserState {
+  users: any[];
+  loading: boolean;
+  error: null | string;
+};
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+export const userReducer = (state = initialState, action:UserAction):UserState => {
+  switch (action.type) {
+    case UserActionTypes.FETCH_USERS:
+      return {loading: true, error: null, users: []};
+    case UserActionTypes.FETCH_USERS_SUCCESS:
+      return {loading: false, error: null, users: action.payload};
+    case UserActionTypes.FETCH_USERS_ERROR:
+      return {loading: false, error: action.payload, users: []};
+    default:
+      return state;
+  }
+};
