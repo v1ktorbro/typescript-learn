bb2a7242af2af00a64be8c3d204b3758ec9a7643
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-08-01 14:22:39 +0500

    feat(user): create action-creater

---
+++ src/store/action-creater/user.ts
@@ -0,0 +1,21 @@
+import { Dispatch } from "redux";
+import { UserAction, UserActionTypes } from "../../types/user";
+import axios from "axios";
+
+export const fetchUsers = () => {
+  return async (dispatch: Dispatch <UserAction>) => {
+    try {
+      dispatch({type: UserActionTypes.FETCH_USERS});
+      const responce = await axios.get('https://jsonplaceholder.typicode.com/users');
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_SUCCESS, 
+        payload: responce.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_ERROR, 
+        payload: 'Произошла ошибка при загрузке пользователей',
+      });
+    };
+  };
+};
