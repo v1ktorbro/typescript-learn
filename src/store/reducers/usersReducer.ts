5d9bb19a9c6136ab98336073e5783a458e5faa9e
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-08-06 18:41:05 +0300

    fix(usersReducer): move reducer logic to index.ts configureStore

--- src/store/reducers/usersReducer.ts
+++ src/store/reducers/usersReducer.ts
@@ -6,7 +6,7 @@ const initialState: UserState = {
   error: null,
 };
 
-export const userReducer = (state = initialState, action:UserAction): UserState => {
+export const usersReducer = (state = initialState, action:UserAction): UserState => {
   switch (action.type) {
     case UserActionTypes.FETCH_USERS:
       return {loading: true, error: null, users: []};
