a276523aec482ebcfa45240026d1edd7404625d1
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-08-08 10:14:28 +0300

    feat(App): connect TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import UserList from './components/UserList';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <UserList />
+      <hr/>
+      <TodoList />
     </div>
   );
 }
